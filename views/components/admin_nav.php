dbe89447fd55bf11f2cb133ca21ef4ece560c620
Rachel Higley <me@rachelhigley.com>
2012-11-12 18:33:35 -0500

    Revert "Basic site setup"
    This reverts commit 9b53c859477a150fab4d298ba8a612bc50e5161e.

--- views/components/admin_nav.php
+++
@@ -1,4 +0,0 @@
-<nav>
-	<a href="<?=Asset::create_url('product','admin')?>">Products</a>
-	<a href="<?=Asset::create_url('order','admin')?>">Orders</a>
-</nav>
--- views/components/rep_nav.php
+++
@@ -1,4 +0,0 @@
-<nav>
-	<a href="<?=Asset::create_url('store','admin')?>">Store Settings</a>
-	<a href="<?=Asset::create_url('order','admin')?>">Orders</a>
-</nav>
