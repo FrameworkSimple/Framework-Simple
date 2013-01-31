b14def872f7759facf4e916c6efeb405d97d252e
Rachel Higley <me@rachelhigley.com>
2013-01-30 21:52:35 -0500

    Adding Setting Templates for future use of the extensions.

---
+++ system/extensions/RussianDollCaching/Settings-Template.php
@@ -0,0 +1,4 @@
+<?php
+
+// the file path and name of where to store cached files
+const CACHE_PATH = "../RDCache";
---
+++ system/extensions/SoftDeletion/Settings-Template.php
@@ -0,0 +1,9 @@
+<?php
+
+// the name of the column to check on
+CONST DELETION_COL_NAME = "date_deleted";
+
+// the type of value to be inserted
+	// timestamp = current timestamp
+	// boolean = 1
+CONST DELETION_TYPE = "timestamp";
