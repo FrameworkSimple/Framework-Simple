98fa13627f48170bc9090d6d0abe92b8707b12ce
Rachel Higley <me@rachelhigley.com>
2013-03-28 13:29:30 -0400

    Basic Scafoliding Extension Setup

---
+++ system/extensions/Scafolding/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+	/**
+	 * Initialize the soft deletion extension
+	 * @category Extensions
+ 	 * @package  Extensions
+ 	 * @subpackage Scafolding
+	 * @author     Rachel Higley <me@rachelhigley.com>
+	 * @copyright  2013 Framework Simple
+	 * @license    http://www.opensource.org/licenses/mit-license.php MIT
+	 * @link       http://rachelhigley.com/framework
+	 */
+
+	Core::add_classes("Scafolding",array(
+		"ScafoldingController"     =>"controllers/ScafoldingController.php",
+		)
+	);
---
+++ system/extensions/Scafolding/views/scafolding/get.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Scafolding</title>
+</head>
+<body>
+	<h1>Scafolding</h1>
+	<form action="<?php Asset::create_url("Scafolding","post");?>" method="POST" enctype="multipart/form-data">
+
+		<label for="name">Name:</label>
+		<input type="text" name="name" id="name">
+
+		<label for="app_name">Application Name</label>
+		<input type="text" name="application_name" id="app_name">
+
+		<label for="sql">Upload Your Database</label>
+		<input type="file" name="sql" id="sql">
+
+		<input type="submit" value="Upload">
+
+	</form>
+</body>
+</html>
